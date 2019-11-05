4f1de14cca4ace9c493a1c46d42fb347803d9ded
zer0231 <49207242+zer0231@users.noreply.github.com>
2019-11-05 16:51:21 +0545

    Update on&off.cpp

--- on&off.cpp
+++ on&off.cpp
@@ -2,10 +2,20 @@
 
 int main()
 	{
-		SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);  //for turning off the monitor
-
-		SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1); //for turning on the monitor
-
+	int i = 0;
+	for(;;){
+		if(i=0){
+			SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);  //for turning off the monitor
+			i = 1;
+			}
+		else if(i=1)
+			{
+			SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1); //for turning on the monitor
+			i=0;
+			}
+		else 
+			cout<<"Nothing happens"<<endl;
+	}
 	return 0;
 	}
 
